e2aca5e4759ce8d1d3b915eff60c5837721fe438
laljiyadav <laljiempire@gmail.com>
2018-03-20 01:43:21 +0545

    Initial commit GITTEST
    Signed-off-by: laljiyadav <laljiempire@gmail.com>

---
+++ app/src/main/java/com/example/erlaljiyadav/mobitask/activites/GitTest.java
@@ -0,0 +1,15 @@
+package com.example.erlaljiyadav.mobitask.activites;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.erlaljiyadav.mobitask.R;
+
+public class GitTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_test);
+    }
+}
